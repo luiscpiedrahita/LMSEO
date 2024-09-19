4efc72c74553d9f3020872b1c8cf508bd10b70b0
luiscpiedrahita <luis1420@gmail.com>
2024-09-19 14:50:36 -0500

    Fixed breadcrumbs broken on mobile devices

--- wp-content/themes/helios/template-parts/breadcrumb/breadcrumb-trail.php
+++ wp-content/themes/helios/template-parts/breadcrumb/breadcrumb-trail.php
@@ -15,7 +15,7 @@ if ( ! function_exists( 'breadcrumb_trail' ) ) {
 	include get_stylesheet_directory() . '/inc/compatibility/class-breadcrumb-trail.php';
 }
 ?>
-<nav class="lmseo-breadcrumb-wrap px-5">
+<nav class="lmseo-breadcrumb-wrap p-0 m-0 px-md-5">
 	<?php
 	breadcrumb_trail( array(
 		'container'   => 'div',
